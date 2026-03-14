9befd9eecb4313df28f25660ce8f5faa758323cd
jordantimeckstrom-eng <jordanteckstrom@proton.me>
2026-03-14 07:15:58 -0700

    Add SpineAlignTime sovereign hybrid v1.3 with PWA support

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'spinealigntime-v1.3';
+const ASSETS = [
+  './SpineAlignTime.html',
+  './manifest.json',
+  './config.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request).catch(() => caches.match('./SpineAlignTime.html')))
+  );
+});
